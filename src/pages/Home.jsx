da69abf929c127c311d4ac5e76f035e6b739ee9e
mahekshah2816 <23dcs119@charusat.edu.in>
2025-06-12 00:26:21 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
 import VideoCard from '../components/VideoCard';
 import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import { fetchVideos } from '../services/youtube';
 import './Home.css';
 
-function Home({ videos, onSearch }) {
+function Home({ onSearch, videos, selectedCategory, setSelectedCategory, setSearchTerm }) {
   return (
     <>
       <Navbar onSearch={onSearch} />
-      <div className="video-list">
-        {videos.map((video) => (
-          <VideoCard key={video.id.videoId || video.id} video={video} />
-        ))}
+      <div className="home-wrapper">
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          setSearchTerm={setSearchTerm}
+        />
+        <div className="home-container">
+          <div className="video-list">
+            {videos.map((video) => (
+              <VideoCard key={video.id.videoId || video.id} video={video} />
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
