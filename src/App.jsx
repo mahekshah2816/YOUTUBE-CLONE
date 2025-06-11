fda034223a37223409dc13fa66408ca6fa4feee8
mahekshah2816 <23dcs119@charusat.edu.in>
2025-06-12 00:16:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-
 import Home from './pages/Home';
 import VideoDetail from './pages/VideoDetail';
 import { fetchVideos } from './services/youtube';
 
 function App() {
   const [videos, setVideos] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('react');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('Home');
 
   useEffect(() => {
-    fetchVideos(searchTerm).then((data) => setVideos(data));
-  }, [searchTerm]);
+    if (searchTerm) {
+      fetchVideos(searchTerm).then((data) => setVideos(data));
+    } else {
+      fetchVideos(selectedCategory).then((data) => setVideos(data));
+    }
+  }, [searchTerm, selectedCategory]);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home videos={videos} onSearch={setSearchTerm} />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              videos={videos}
+              onSearch={setSearchTerm}
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+              setSearchTerm={setSearchTerm}
+            />
+          }
+        />
         <Route path="/video/:id" element={<VideoDetail />} />
       </Routes>
     </BrowserRouter>
